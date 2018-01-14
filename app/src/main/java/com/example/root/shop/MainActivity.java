0eb444493f8052c7aac6f0b7c50ff4b4761b2392
root <KarimAshafElshaweish@gmail.com>
2018-01-15 00:21:12 +0200

    frist commit

---
+++ app/src/main/java/com/example/root/shop/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.root.shop;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.res.ColorStateList;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            android.support.v4.app.FragmentManager  fragmentManager=getSupportFragmentManager();
+            android.support.v4.app.FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+            switch (item.getItemId()) {
+                case R.id.navigation_categpory:
+                    fragmentTransaction.replace(R.id.content,new Category()).commit();
+                    return  true;
+                case R.id.navigation_home:
+                    fragmentTransaction.replace(R.id.content,new Home()).commit();
+                    return true;
+                case R.id.navigation_account:
+                    fragmentTransaction.replace(R.id.content,new Profile()).commit();
+                    return true;
+                case R.id.navigation_shop:
+                    fragmentTransaction.replace(R.id.content,new ShopFragment()).commit();
+                    return true;
+                case R.id.navigation_likes:
+                    fragmentTransaction.replace(R.id.content,new Likes()).commit();
+                    return true;
+
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation =findViewById(R.id.navigation);
+        navigation.setItemIconTintList(null);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        android.support.v4.app.FragmentManager  fragmentManager=getSupportFragmentManager();
+        android.support.v4.app.FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.content,new Category()).commit();
+
+    }
+
+}
---
+++ app/src/main/java/com/example/root/shop/Profile.java
@@ -0,0 +1,138 @@
+package com.example.root.shop;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link Profile.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link Profile#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Profile extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    Context ctx;
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public Profile() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Profile.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Profile newInstance(String param1, String param2) {
+        Profile fragment = new Profile();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_profile, container, false);
+        final android.support.v7.widget.AppCompatTextView editText_info=(android.support.v7.widget.AppCompatTextView)view.findViewById(R.id.info_text);
+        editText_info.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                final int DRAWABLE_LEFT = 0;
+                final int DRAWABLE_TOP = 1;
+                final int DRAWABLE_RIGHT = 2;
+                final int DRAWABLE_BOTTOM = 3;
+                if(motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    if(motionEvent.getX() <= (editText_info.getCompoundDrawables()[DRAWABLE_LEFT].getBounds().width()))                         // your action here
+                    {
+                        Fragment fragment=new information();
+                        FragmentManager fragmentManager=getActivity().getSupportFragmentManager();
+                       fragmentManager.beginTransaction().replace(R.id.content,fragment).commit();
+                        Toast.makeText(ctx, "her", Toast.LENGTH_SHORT).show();
+                    }
+                        return true;
+                    }
+                return true;
+
+            }
+        });
+        return view;
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        ctx=context;
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            Toast.makeText(context, "حسابى", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+
+}
